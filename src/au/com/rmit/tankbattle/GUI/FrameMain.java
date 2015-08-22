f8b90c2869fbbb5a97281710eaa32f82337f359e
Ricol Wang <wangxinghe1983@gmail.com>
2015-08-22 16:59:48 +1000

    update.

--- src/au/com/rmit/tankbattle/GUI/FrameMain.java
+++ src/au/com/rmit/tankbattle/GUI/FrameMain.java
@@ -5,8 +5,8 @@ import au.com.rmit.tankbattle.scene.TankBattleScene;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
+import static java.lang.System.exit;
 import java.util.Random;
-import javax.swing.SwingUtilities;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -39,6 +39,8 @@ public class FrameMain extends javax.swing.JFrame implements KeyListener
     {
 
         panelGame = new javax.swing.JPanel();
+        btnStart = new javax.swing.JButton();
+        btnClose = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         addComponentListener(new java.awt.event.ComponentAdapter()
@@ -70,8 +72,26 @@ public class FrameMain extends javax.swing.JFrame implements KeyListener
         );
         panelGameLayout.setVerticalGroup(
             panelGameLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 565, Short.MAX_VALUE)
+            .addGap(0, 524, Short.MAX_VALUE)
         );
+
+        btnStart.setText("Start");
+        btnStart.addActionListener(new java.awt.event.ActionListener()
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                btnStartActionPerformed(evt);
+            }
+        });
+
+        btnClose.setText("Close");
+        btnClose.addActionListener(new java.awt.event.ActionListener()
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                btnCloseActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -79,13 +99,23 @@ public class FrameMain extends javax.swing.JFrame implements KeyListener
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(panelGame, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(panelGame, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(btnStart)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnClose)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnClose)
+                    .addComponent(btnStart))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(panelGame, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
@@ -100,48 +130,6 @@ public class FrameMain extends javax.swing.JFrame implements KeyListener
 
     private void formWindowActivated(java.awt.event.WindowEvent evt)//GEN-FIRST:event_formWindowActivated
     {//GEN-HEADEREND:event_formWindowActivated
-        // TODO add your handling code here:
-
-        if (bAlreadyRun)
-        {
-            return;
-        }
-
-        bAlreadyRun = true;
-
-        new Thread(new Runnable()
-        {
-
-            @Override
-            public void run()
-            {
-                try
-                {
-                    Thread.sleep(500);
-                } catch (InterruptedException ex)
-                {
-                }
-
-                SwingUtilities.invokeLater(new Runnable()
-                {
-                    public void run()
-                    {
-                        theScene = new TankBattleScene();
-                        Director.getSharedInstance().showScene(theScene);
-
-                        try
-                        {
-                            Thread.sleep(500);
-                        } catch (InterruptedException ex)
-                        {
-                        }
-                        theScene.gameStart();
-                    }
-                });
-
-            }
-
-        }).start();
     }//GEN-LAST:event_formWindowActivated
 
     private void formComponentResized(java.awt.event.ComponentEvent evt)//GEN-FIRST:event_formComponentResized
@@ -150,7 +138,30 @@ public class FrameMain extends javax.swing.JFrame implements KeyListener
             theScene.adjustLabelPos();
     }//GEN-LAST:event_formComponentResized
 
+    private void btnStartActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_btnStartActionPerformed
+    {//GEN-HEADEREND:event_btnStartActionPerformed
+        if (theScene == null)
+        {
+            theScene = new TankBattleScene();
+            Director.getSharedInstance().showScene(theScene);
+            theScene.gameStart();
+        } else
+            theScene.pause();
+
+        if (theScene.isScenePaused())
+            btnStart.setText("Continue");
+        else
+            btnStart.setText("Pause");
+    }//GEN-LAST:event_btnStartActionPerformed
+
+    private void btnCloseActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_btnCloseActionPerformed
+    {//GEN-HEADEREND:event_btnCloseActionPerformed
+        exit(0);
+    }//GEN-LAST:event_btnCloseActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnClose;
+    private javax.swing.JButton btnStart;
     private javax.swing.JPanel panelGame;
     // End of variables declaration//GEN-END:variables
 
